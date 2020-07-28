37eedf2c0b5d8bb98757e270853a1744eeddc59f
jenniferoenning <oenningstangjennifer@gmail.com>
2020-07-28 20:12:38 -0300

    added logo jenni in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import Logo from '../../assets/img/Logo.png';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://github.com/jenniferoenning">
+        <img className="Logo" src={Logo} alt="Logo JenniFlix" />
       </a>
       <p>
         Orgulhosamente criado durante a
